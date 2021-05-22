d54d9001cbcd599299218f6c3660724665fe533f
Nandakumar T <nandakumart07@gmail.com>
2021-05-22 17:00:33 +0530

    react sample-Hooks,useState,Spread Operator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-
-import './app.css'
-import Header from './components/Header'
-
+import {useState} from 'react'
+//import Counter from './Counter'
+import Employee from './Employee'
 function App() {
-  const data='Nandakumar'
+  const [count,setCount]=useState(0)
+  const addCount=()=>{
+    setCount(count+1)
+    console.log(count)
+  }
+  // let obj = {
+  //   title:'1st counter',
+  //   count
+  //   ,
+  //   place:'hello'
+  // }
+  let emp=[{name:'nandu',age:'15'},
+  {name:'musk',age:'21'},
+  {name:'tata',age:'25'}
+]
   return (
-    <div>
-    <Header data={data}/>
-    <p className='hello'>sample {data}</p>
-    
+    <div className="App">
+      <button onClick={addCount}>Add</button>
+      {/* <Counter {...obj}/>
+      <Counter title='2nd' count={count}/>
+     */}
+     {
+       emp.map((obj,index)=>
+        //  (<div>
+          //  <h1>Hello</h1>
+           <Employee key={index} {...obj}/>
+          //  </div>)
+       )
+     }
     <br/>
     </div>
   );
