c672d57b7784ba2ac1227a296efcaeff8b5f6f25
Nandakumar T <nandakumart07@gmail.com>
2021-05-23 16:59:35 +0530

    react sample-part:4-useEffect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
+// import {useState} from 'react'
+// //import Counter from './Counter'
+// import Employee from './Employee'
+// function App() {
+//   const [count,setCount]=useState(0)
+//   const addCount=()=>{
+//     setCount(count+1)
+//     console.log(count)
+//   }
+//   // let obj = {
+//   //   title:'1st counter',
+//   //   count
+//   //   ,
+//   //   place:'hello'
+//   // }
+//   let emp=[{name:'nandu',age:'15'},
+//   {name:'musk',age:'21'},
+//   {name:'tata',age:'25'}
+// ]
+//   return (
+//     <div className="App">
+//       <button onClick={addCount}>Add</button>
+//       {/* <Counter {...obj}/>
+//       <Counter title='2nd' count={count}/>
+//      */}
+//      {
+//        emp.map((obj,index)=>
+//         //  (<div>
+//           //  <h1>Hello</h1>
+//            <Employee key={index} {...obj}/>
+//           //  </div>)
+//        )
+//      }
+//     <br/>
+//     </div>
+//   );
+// }
+
+//import counter from "./counter";
+
+// export default App;
+import Counter from './Counter';
 import {useState} from 'react'
-//import Counter from './Counter'
-import Employee from './Employee'
-function App() {
-  const [count,setCount]=useState(0)
-  const addCount=()=>{
-    setCount(count+1)
-    console.log(count)
-  }
-  // let obj = {
-  //   title:'1st counter',
-  //   count
-  //   ,
-  //   place:'hello'
-  // }
-  let emp=[{name:'nandu',age:'15'},
-  {name:'musk',age:'21'},
-  {name:'tata',age:'25'}
-]
-  return (
-    <div className="App">
-      <button onClick={addCount}>Add</button>
-      {/* <Counter {...obj}/>
-      <Counter title='2nd' count={count}/>
-     */}
-     {
-       emp.map((obj,index)=>
-        //  (<div>
-          //  <h1>Hello</h1>
-           <Employee key={index} {...obj}/>
-          //  </div>)
-       )
-     }
-    <br/>
-    </div>
-  );
+function App(){
+  const [state,setState]=useState(false)
+  return(
+    <div className='App'> 
+    <h1 onClick={()=>setState(!state)}>show/hide</h1>
+    { state &&<Counter/> }
+      </div>
+  )
 }
 
-export default App;
-
-
+export default App
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+// import React from 'react'
 
-function Counter({title,...obj}) {
+// function Counter({title,...obj}) {
      
-    return (
+//     return (
         
+//         <div>
+//             <h1>{title}{obj.count}{obj.place}</h1>
+//         </div>
+//     )
+// }
+
+// export default Counter
+
+import React,{useState,useEffect} from 'react'
+
+function Counter() {
+    const [count,setCount]=useState(0)
+    const [count2,setCount2]=useState(0)
+    useEffect(()=>{
+        console.log('mounting..')
+        console.log('count1..'+count)
+        console.log('count2..'+count2)
+        
+        
+    },[count,count2])
+    return (
         <div>
-            <h1>{title}{obj.count}{obj.place}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <h1>Hello I am component:{count}</h1>
+
+            <button onClick={()=>setCount2(count2+1)}>Increment</button>
+            <h1>Hello I am component 2:{count2}</h1>
         </div>
     )
 }
 
 export default Counter
+
